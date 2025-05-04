1193426dd41dc832c7c6b1366bb25a1f57e44706
lakshitthakur <lakshitthakur224@gmail.com>
2025-05-04 23:47:02 +1000

    script.js

---
+++ Day12_ToDoListApp/Web/script.js
@@ -0,0 +1,15 @@
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("done");
+  });
+
+  document.getElementById("taskList").appendChild(li);
+  taskInput.value = "";
+}
